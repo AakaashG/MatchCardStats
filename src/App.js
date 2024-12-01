a3af0a933648d3a4ab5dcdc9d4131ceea6e410a2
aakaashg <aakaash.r.ghante@gmail.com>
2024-12-01 14:48:05 -0500

    imported flask for backend and created an input/submit thing for frontend

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [eventCode, setEventCode] = useState("fgdfgd");
+  const [isLoading, setIsLoading] = useState(false);
+  const [isEntered, setIsEntered] = useState(false);
+  const [text, setText] = useState("");
+  const [data, setData] = useState([]);
+
+  const handleChange = ({target}) => {
+    setText(target.value);
+    setEventCode(target.value);
+  }
+
   return (
     <div>
-      
+      <form onSubmit={(e)=> {
+        e.preventDefault();
+        setText("");
+      }}>
+        <input placeholder="Enter event code" value={text} onChange={handleChange}>
+        </input>
+        <button type="submit">Enter</button>
+      </form>
+      <h1>{eventCode}</h1>
+      <table>
+      </table>
     </div>
   );
 }
