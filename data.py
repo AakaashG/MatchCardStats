2c5ed63474df966cdc17bddfefaf02ed2b3e76e0
aakaashg <aakaash.r.ghante@gmail.com>
2024-12-01 18:12:19 -0500

    forgot to commit before; made lots of changes: deleted back-end server after fixing api call in front-end, made lots of useState variables to track data, displayed data in tables, and updated error catching (not finished) in the getData function

--- data.py
+++ data.py
@@ -1,1 +1,21 @@
-import flask
+"""
+Fixed the frontend server so no need for this
+"""
+# from flask import Flask, jsonify, request;
+# import statbotics
+# from flask_cors import CORS;
+
+# app = Flask(__name__)
+# CORS(app)
+# sb = statbotics.Statbotics();
+
+# @app.route('/', methods=['POST'])
+# def post_data():
+#     eventKey = request.get_json().get("key")
+#     print(eventKey)
+#     events = sb.get_event(eventKey);
+#     print("Hello world!")
+#     return jsonify(events)
+
+# if __name__ == '__main__':
+#     app.run(debug=True, host='0.0.0.0', port=5000)
--- src/App.js
+++ src/App.js
@@ -7,10 +7,61 @@ function App() {
   const [isEntered, setIsEntered] = useState(false);
   const [text, setText] = useState("");
   const [data, setData] = useState([]);
+  const [index, setIndex] = useState(0);
 
   const handleChange = ({target}) => {
     setText(target.value);
     setEventCode(target.value);
+  }
+
+  const handleClick = ({target}) => {
+    setIndex(index + parseInt(target.value));
+  };
+
+  const getData = async () => {
+    setIsEntered(true);
+    setIsLoading(true);
+    try {
+      const response = await fetch(`https://api.statbotics.io/v2/matches/event/${eventCode}`, {
+      })
+      if (!response.ok) {
+        throw new Error(`Error: ${response.status}`);
+      }else {
+        setData(await response.json());
+        console.log(data);
+      }
+    } catch(error) {
+      console.log(error);
+    }finally {
+      setIsLoading(false);
+    }
+  }
+  
+  const displayData = (index) => {
+    console.log(data);
+    let temp = data[index];
+    console.log(temp);
+    console.log("HELLO!");
+    return (
+      <>
+      <tr>
+        <th>Team Red</th>
+        <th>{isLoading ? "Loading..." : temp.red_auto}</th>
+        <th>{isLoading ? "Loading..." : temp.red_teleop}</th>
+        <th>{isLoading ? "Loading..." : temp.red_endgame}</th>
+        <th>{isLoading ? "Loading..." : temp.red_fouls}</th>
+        <th>{isLoading ? "Loading..." : temp.red_fouls+temp.red_endgame+temp.red_teleop+temp.red_auto}</th>
+      </tr>
+      <tr>
+        <th>Team Blue</th>
+        <th>{isLoading ? "Loading..." : temp.blue_auto}</th>
+        <th>{isLoading ? "Loading..." : temp.blue_teleop}</th>
+        <th>{isLoading ? "Loading..." : temp.blue_endgame}</th>
+        <th>{isLoading ? "Loading..." : temp.blue_fouls}</th>
+        <th>{isLoading ? "Loading..." : temp.blue_fouls+temp.blue_endgame+temp.blue_teleop+temp.blue_auto}</th>
+      </tr>
+      </>
+    )
   }
 
   return (
@@ -18,14 +69,40 @@ function App() {
       <form onSubmit={(e)=> {
         e.preventDefault();
         setText("");
+        getData();
       }}>
         <input placeholder="Enter event code" value={text} onChange={handleChange}>
         </input>
         <button type="submit">Enter</button>
       </form>
       <h1>{eventCode}</h1>
-      <table>
-      </table>
+      <div className="flex">
+        <button 
+        onClick={handleClick} 
+        value={-1}
+        disabled={index == 0 ? true : false}
+        > left </button>
+        <table>
+          <tr>
+            <th>Alliance</th>
+            <th>Auto Score</th>
+            <th>Teleop Score</th>
+            <th>Endgame Score</th>
+            <th>Fouls</th>
+            <th>Total Score</th>
+            <th>Net EPA</th>
+            <th>Alliance</th>
+          </tr>
+          {isEntered ? <tbody>{displayData(index)}</tbody> : ''}
+          
+        </table>
+        <button 
+        onClick={handleClick} 
+        value={1}
+        disabled={index === data.length ? true : false}
+        > right </button>
+        <h1>{index}</h1>
+      </div>
     </div>
   );
 }
